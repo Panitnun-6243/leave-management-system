e7c3f01d999a36b7797eb8bfa3d7f3c951d32fd2
Panitnun-6243 <panitnun.6243@gmail.com>
2024-09-20 20:32:00 +0700

    feat: separate component and state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,46 +6,70 @@ interface Todo {
 	title: string
 }
 
-export default function Home() {
-	const [todos, setTodos] = useState<Todo[]>([])
+interface TodoFormProps {
+	onSubmit: (input: string) => void
+}
+
+interface TodoListProps {
+	todos: Todo[]
+}
+
+function TodoForm({ onSubmit }: TodoFormProps) {
 	const [input, setInput] = useState('')
 
 	function handleChange(event: React.FormEvent<HTMLInputElement>) {
 		setInput(event.currentTarget.value)
 	}
 
-	function handleSubmit(event: React.MouseEvent) {
-		event.preventDefault()
+	function handleSubmit(e: React.FormEvent) {
+		e.preventDefault()
+		onSubmit(input)
+		setInput('')
+	}
+
+	return (
+		<form onSubmit={(e) => handleSubmit(e)}>
+			<label htmlFor="todo"></label>
+			<input
+				required
+				type="text"
+				id="todo"
+				value={input}
+				onChange={handleChange}
+				className="border border-red-500"
+			/>{' '}
+			<button type="submit">submit</button>
+		</form>
+	)
+}
+
+function TodoList({ todos }: TodoListProps) {
+	return (
+		<ul>
+			{todos.map((item) => (
+				<li key={item.id}>Todo: {item.title}</li>
+			))}
+		</ul>
+	)
+}
+
+export default function Home() {
+	const [todos, setTodos] = useState<Todo[]>([])
+
+	function handleSubmit(input: string) {
 		const newTodo: Todo = {
 			id: +new Date(),
 			title: input,
 		}
 		setTodos([...todos, newTodo])
-		setInput('')
 	}
 
 	return (
 		<>
 			<header>Todo App</header>
-			<form>
-				<label htmlFor="todo"></label>
-				<input
-					type="text"
-					id="todo"
-					value={input}
-					onChange={handleChange}
-					required
-					className="border border-red-500"
-				/>{' '}
-				<button onClick={handleSubmit}>submit</button>
-			</form>
-
+			<TodoForm onSubmit={handleSubmit} />
 			<br />
-			<ul>
-				{todos.map((item) => (
-					<li key={item.id}>Todo: {item.title}</li>
-				))}
-			</ul>
+			<TodoList todos={todos} />
 		</>
 	)
 }
