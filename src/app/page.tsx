505c04d5c567e739320a252567c9b7e016f21cf7
Panitnun-6243 <panitnun.6243@gmail.com>
2024-10-28 13:51:11 +0700

    feat: add useeffect example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
 'use client'
 
-import TodoForm from '@/features/todo/components/TodoForm'
-import TodoList from '@/features/todo/components/TodoList'
-import { type Todo } from '@/features/todo/types'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-	const [todos, setTodos] = useState<Todo[]>([])
+	const [num, setNum] = useState(0)
+	useEffect(() => {
+		console.log('Use effect called', num)
 
-	function handleSubmit(input: string) {
-		const newTodo: Todo = {
-			id: +new Date(),
-			title: input,
+		// Unmounting ช่วงเวลาสุดท้ายก่อนเริ่ม useEffect ใหม่ หรือก่อน component หายไป (clean up)
+		return () => {
+			console.log('Bye', num)
 		}
-		setTodos([...todos, newTodo])
+	}, [num])
+
+	function handleClick() {
+		setNum(num + 1)
+		console.log(num)
 	}
 
 	return (
 		<>
-			<header>Todo App</header>
-			<TodoForm onSubmit={handleSubmit} />
-			<br />
-			<TodoList todos={todos} />
+			<div>HomePage</div>
+			<button onClick={handleClick}>Click me</button>
+			<div>{num}</div>
 		</>
 	)
 }
---
+++ src/app/todo/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import TodoForm from '@/features/todo/components/TodoForm'
+import TodoList from '@/features/todo/components/TodoList'
+import { type Todo } from '@/features/todo/types'
+import { useState } from 'react'
+
+export default function Todo() {
+	const [todos, setTodos] = useState<Todo[]>([])
+
+	function handleSubmit(input: string) {
+		const newTodo: Todo = {
+			id: +new Date(),
+			title: input,
+		}
+		setTodos([...todos, newTodo])
+	}
+
+	return (
+		<>
+			<header>Todo App</header>
+			<TodoForm onSubmit={handleSubmit} />
+			<br />
+			<TodoList todos={todos} />
+		</>
+	)
+}
